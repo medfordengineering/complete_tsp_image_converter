b98d67396e3ae6481390c3785adecbfb6e1a9582
Sam Christy <schristy@medford.k12.ma.us>
2020-04-29 12:30:43 -0400

    Added all of the TSP code

--- plotter_control.py
+++ plotter_control.py
@@ -1,9 +1,21 @@
+from __future__ import print_function
+import math
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os
+import sys
+import serial
+import json
+import time
+
 app = Flask(__name__)
 
 coordinates = []
+#data = {}
+#data['points'] = 0
 
 def convert(list):
 	return tuple(list)
@@ -27,9 +39,17 @@ def uploaded_file():
 		command = 'convert {} {}'.format('static/' + outfile, 'static/' + viewfile)
 		os.system(command)
 		data = create_data_model('static/' + outfile)
-		print(data['points'])
+#		create_data_model('static/' + outfile)
 		return render_template('uploaded.html', user_image = viewfile, points = data['points'])
 
+@app.route('/process', methods = ['GET', 'POST'])
+def process_file():
+	if request.method == 'POST':
+		data = request.form.get('data')
+		print(data)
+
+#Copied from forums should avoid cacheing. Have not tested if it actually works.
+#Have some evidenced that this might work but need a cache view to be sure
 @app.after_request
 def add_header(r):
 	r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -76,8 +96,75 @@ def create_data_model(filepath):
 	data['points'] = points
 	return data
 
+def compute_euclidean_distance_matrix(locations):
+	"""Creates callback to return distance between points."""
+	distances = {}
+	for from_counter, from_node in enumerate(locations):
+		distances[from_counter] = {}
+		for to_counter, to_node in enumerate(locations):
+			if from_counter == to_counter:
+				distances[from_counter][to_counter] = 0
+			else:
+				# Euclidean distance
+				distances[from_counter][to_counter] = (int(
+					math.hypot((from_node[0] - to_node[0]),
+							   (from_node[1] - to_node[1]))))
+	return distances
 
 
+def print_solution(manager, routing, solution):
+	data_pairs = {}	
+	ser = serial.Serial('/dev/ttyUSB0', 9600)
+	# Arduino needs time to start after reset
+	time.sleep(2)
+	index = routing.Start(0)
+	coordinates_route = []
+	while not routing.IsEnd(index):
+		coordinates_route.append(coordinates[manager.IndexToNode(index)])
+		data_pairs = json.dumps({"xy":coordinates[manager.IndexToNode(index)]})
+		print(data_pairs)
+		ser.write(data_pairs.encode('ascii'))
+		index = solution.Value(routing.NextVar(index))
+		time.sleep(.1)
+
+def last():
+	"""Entry point of the program."""
+	# Instantiate the data problem.
+	data = create_data_model()
+
+	# Create the routing index manager.
+	manager = pywrapcp.RoutingIndexManager(len(data['locations']),
+										   data['num_vehicles'], data['depot'])
+	# Create Routing Model.
+	routing = pywrapcp.RoutingModel(manager)
+
+	distance_matrix = compute_euclidean_distance_matrix(data['locations'])
+
+	def distance_callback(from_index, to_index):
+		"""Returns the distance between the two nodes."""
+		# Convert from routing variable Index to distance matrix NodeIndex.
+		from_node = manager.IndexToNode(from_index)
+		to_node = manager.IndexToNode(to_index)
+		return distance_matrix[from_node][to_node]
+
+	transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+	# Define cost of each arc.
+	routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+	# Setting first solution heuristic.
+	search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+	search_parameters.first_solution_strategy = (
+		routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+	# Solve the problem.
+	#THIS IS THE BOTTLE NECK
+	solution = routing.SolveWithParameters(search_parameters)
+	print("solution")
+
+	# Print solution on console.
+	if solution:
+		print_solution(manager, routing, solution)
 
 if __name__ == '__main__':
 	app.run(debug = True)
